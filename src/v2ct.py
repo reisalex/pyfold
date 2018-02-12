5f91cb981352d507a5f0187f65375b10d57ab8a3
alex.reis <areis1992@gmail.com>
2018-02-11 19:00:16 -0500

    Now using v2ct and ct2v as independent fxns. Should rename module.

--- src/v2ct.py
+++ src/v2ct.py
@@ -2,80 +2,79 @@
 Subroutine: V2CT (IBSP,FLD,JOB,N)
 
 Description: Converts a secondary structure from either Vienna format
-             to CT format, or CT to Vienna.
+       to CT format, or CT to Vienna.
 
 Arguments:
 
-          IBSP - Base-pair information for the RNA.
-           FLD - Vienna fold for the RNA.
-           JOB - Conversion job to perform.
-                 JOB = 'V' Convert to Vienna Format
-                 JOB = 'C' Convert to CT Format.
-             N - Number of nucleotides in the RNA.
+      IBSP - Base-pair information for the RNA.
+       FLD - Vienna fold for the RNA.
+       JOB - Conversion job to perform.
+         JOB = 'V' Convert to Vienna Format
+         JOB = 'C' Convert to CT Format.
+       N - Number of nucleotides in the RNA.
 
 History:
 Version     Date            Comment
 --------    -------         --------------------
-            09/28/2017      Original Code
+      09/28/2017      Original Code
 
 Dependencies:
 
 Author(s): Alex Reis
-           Copyright (c) 2017 (Please refer to LICENCE)
+       Copyright (c) 2017 (Please refer to LICENCE)
 """
 
-def V2CT(ibsp=[],fld=[],job,n):
+def V2CT(fld):
 
-    # INTEGERS
-    # n,ibsp(n),i,j,count,istack(n)
+  # INTEGERS
+  # n,ibsp(n),i,j,count,istack(n)
 
-    # STRING
-    # job,fld(n)
+  # STRING
+  # fld(n)
 
-    #=== Convert to Vienna ===#
-    if ( job == "V" ):
+  n = len(fld)
 
-        assert (not ibsp is None), "Make sure to provide ibsp for job 'V'."
+  s = 0
 
-        for i in range(0,n):
+  ibsp   = [0]*n
+  istack = [0]*n
 
-            if ( ibsp[i] == 0 ):
-                fld[i] = '.'
+  for i in range(0,n):
 
-            else:
-                j = ibsp[i]
+    if fld[i] == '.':
+      ibsp[i] = 0
 
-                if ( j > i ):
-                    fld[i] = '('
-                    fld[j] = ')'
-                else:
-                    fld[i] = ')'
-                    fld[j] = '('
+    elif fld[i] == '(':
+      s += 1
+      istack[s] = i
 
+    elif fld[i] == ')':
+      j = istack[s]
+      ibsp[j] = i
+      ibsp[i] = j
+      s -= 1
 
+  return ibsp
 
-    #=== Convert to CT ===#
-    if ( job == "C" ):
+def CT2V(ibsp,n):
 
-        assert (not fld is None), "Make sure to provide fld for job 'C'."
+  # INTEGERS
+  # n,ibsp(n),i,j,count,istack(n)
 
-        count = 0
-        ibsp[:] = 0
-        istack[:] = 0
+  # STRING
+  # fld(n)
 
-        for i in range(0,n):
+  for i in range(0,n):
 
-            if fld[i] == '.':
-                ibsp[i] = 0
+    if ( ibsp[i] == 0 ):
+      fld[i] = '.'
+    else:
+      j = ibsp[i]
+      if ( j > i ):
+        fld[i] = '('
+        fld[j] = ')'
+      else:
+        fld[i] = ')'
+        fld[j] = '('
 
-            elif fld[i] == '(':
-                count += 1
-                istack[count] = i
-
-            elif fld[i] == ')':
-                j = istack[count]
-                ibsp[j] = i
-                ibsp[i] = j
-                count -= 1
-
-    return ibsp,fld
+  return fld
